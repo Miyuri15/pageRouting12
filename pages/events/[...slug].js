9064021b9cee432916630502260dacadd3d2245a
Miyuri15 <miyurilokuhewage15@gmail.com>
2024-10-10 05:53:31 +0530

    fkhg

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,22 @@
+import { useRouter } from "next/router";
+
 function FilteredEventsPage(){
+    const router = useRouter();
+
+    const filterData = router.query.slug;
+
+    if(!filterData){
+        return<p className="center">Loading...</p>
+    }
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if(isNaN(numYear) || isNan(numMonth) || numYear>2030 || numYear < 2021 || numMonth < 1 || numMonth >12){
+        return <p>Invalid filter, Please adjust your values...</p>
+    }
 return(
     <div>
         <h1>Filtered Events</h1>
